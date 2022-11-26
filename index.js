789bb251f35dbee56df1dd4a7b51bbeb7effe952
Maryann Ruguru Njuguna <maryannruguru20@gmail.com>
2022-11-26 12:51:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,72 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = 62;
+function twoNum(multiply){
+  num1*num2;
+  return multiply;
+
+}
+let random = 1;
+function num(random){
+  random() * max | 0;
+  Number .isInteger(random);
+  return num;
+
+}
+let num3 = 19;
+let num4 = 5;
+let mod = 4;
+function number(mod){
+  num3%num4
+  return mod;
+}
+
+let max = 20;
+function maxNum(max){
+Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
+return max;
+}
+
+
+//describe('max', function() {
+  //it("will return 20 as the highest number in the set", function() {
+  //  expect(max).to.eq(20)
+  //});
+//describe('mod', function() {
+ // it("is an equation that calculates a remainder that is equal to 4", function() {
+ //   expect(num3).to.be.a('number')
+  //  expect(num4).to.be.a('number')
+  //  expect(mod).to.eq(4)
+ // });
+
+
+//escribe('random', function() {
+ // it("generates a random integer greater than 0", function() {
+ //   expect(Number.isInteger(random)).to.be.true
+ //   expect(random).to.not.eq(0)
+ // });
+//describe('multiply', function() {
+ // it("is an equation whose multiplied results will equal 62", function() {
+  //  expect(num1).to.be.a('number')
+  //  expect(num2).to.be.a('number')
+  //  expect(multiply).to.eq(62)
+  //});
+   // describe('scuberGreetingForFeet()', function () {
+   //     it('gives customers a free sample if the ride is less than or equal to 400 feet', function () {
+    //      expect(scuberGreetingForFeet(199)).to.equal('This one is on me!');
+   //     });
+   //     unction scuberGreetingForFeet(dist){
+    //      // Write your code here!
+   //       if (dist <= 400){
+   //         return "This one is on me!";
+   //       }
+    //      else if (dist > 200 && dist < 2500){
+  //          return "I will gladly take your thirty bucks.";
+   //       }
+   //       else if(dist > 2500){
+   //         return "No can do.";
+   //       }
+ //         }
+        
